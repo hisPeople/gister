d89a0e7b19a2c28ab5427d371b0958d4f8dd8149
Devon Moss <dvnmss@gmail.com>
2013-11-25 08:59:35 -0700

    added selectable languages. syntax highlighting changes with new language selection

--- background.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: devon
- * Date: 11/24/13
- * Time: 11:28 AM
- * To change this template use File | Settings | File Templates.
- */
--- popup.js
+++ popup.js
@@ -1,13 +1,29 @@
-var gists = angular.module('gistApp', ['ngResource']);
+var gists = angular.module('gistApp', ['ngResource', 'ui.codemirror']);
 
 gists.factory('Gist', function($resource) {
 	return $resource('https://api.github.com/gists',{})
 });
 
 gists.controller('AppCtrl', ['$scope', 'Gist', function($scope, Gist) {
+
+	$scope.modes = ['Python', 'Java', 'Javascript', 'Ruby', 'Markdown'];
+	$scope.mode = $scope.modes[0];
+
 	$scope.description = '';
 	$scope.filename = '';
 	$scope.content = '';
+	console.log($scope.mode)
+
+	$scope.editorOptions = {
+		lineNumbers: true,
+		onLoad: function(cm) {
+			$scope.modeChanged = function() {
+				console.log($scope.mode)
+				cm.setOption("mode", $scope.mode.toLowerCase());
+			};
+		}
+	};
+
 	$scope.filenamestring = function() {
 		return '"' + $scope.filename + '"';
 	} 
@@ -15,7 +31,7 @@ gists.controller('AppCtrl', ['$scope', 'Gist', function($scope, Gist) {
 	$scope.savegist = function() {
 		console.log('clicked')
 		var file = {};
-		file[$scope.filename] = {"content": cmirror.getValue()}
+		file[$scope.filename] = {"content": $scope.content}
 
 		var gist = new Gist();
 		gist.description = $scope.description;
@@ -28,17 +44,17 @@ gists.controller('AppCtrl', ['$scope', 'Gist', function($scope, Gist) {
 	}
 }]);
 
-window.onload = function() {
-	defLang = localStorage["default_language"];
-	console.log(localStorage)
-	console.log(defLang)
-	var modescript = document.createElement("script");
-	modescript.type = "text/javascript"
-	modescript.src = "codemirror/mode/" + defLang + "/" + defLang + ".js"
-	document.body.appendChild(modescript)
-	cmirror = CodeMirror.fromTextArea(codecontent, {
-	     mode: "python",
-	     lineNumbers: true
-    });
-    $(cmirror).focus()
-}
+// window.onload = function() {
+// 	defLang = localStorage["default_language"];
+// 	console.log(localStorage)
+// 	console.log(defLang)
+// 	var modescript = document.createElement("script");
+// 	modescript.type = "text/javascript"
+// 	modescript.src = "codemirror/mode/" + defLang + "/" + defLang + ".js"
+// 	document.body.appendChild(modescript)
+// 	cmirror = CodeMirror.fromTextArea(codecontent, {
+// 	     mode: "python",
+// 	     lineNumbers: true
+//     });
+//     $(cmirror).focus()
+// }
